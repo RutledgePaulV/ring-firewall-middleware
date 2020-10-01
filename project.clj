73c251ffa326fbf79e308a351c2e7e0ec725c8ce
Paul Rutledge <paul.rutledge@rate.com>
2020-10-01 08:47:48 -0500

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/ring-firewall-middleware "0.1.1"
+(defproject org.clojars.rutledgepaulv/ring-firewall-middleware "0.1.2-SNAPSHOT"
 
   :description
   "A library for filtering ring requests by client ip address."
